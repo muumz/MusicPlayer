9d99da8a431f2d2d4800e8283e0cc99b6bd2fcfe
ephrinaw <39829776+ephrinaw@users.noreply.github.com>
2021-10-28 20:39:21 +0300

    primary comit
    almost complete

---
+++ app/misc/color.js
@@ -0,0 +1,9 @@
+export default {
+    APP_BG: '#CBC3E3',
+    FONT: '#010f2c',
+    FONT_MEDIUM: '#636363',
+    FONT_LIGHT: 'white',
+    MODAL_BG: 'rgba(132, 115, 172, 0.6)',
+    ACTIVE_BG: '#1c032ce0',
+    ACTIVE_FONT: '#dda0dd',
+  };
